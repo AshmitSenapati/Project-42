1819dfbe46e2dee72f6f2248cae940873e00a54e
AshmitSenapati <71175726+AshmitSenapati@users.noreply.github.com>
2022-04-28 07:33:50 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,38 @@
+class Drops {
+    /*
+     constructor(x,y){
+         var options = {
+             isStatic: false,
+         }
+         this.rain = Bodies.circle(x,y,5,options)
+         World.add(world, this.rain)
+     }
+     */
+     constructor(x,y){
+         var options = {
+             friction: 0.001,
+             restitution:0.1           
+         }
+         this.rain = Bodies.circle(x,y,5,options)
+         this.radius = 5;
+         World.add(world, this.rain);
+     }
+ 
+ 
+     update(){
+         if(this.rain.position.y > height){
+ 
+             Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+         }
+     }
+     display(){
+         fill("blue")
+         ellipseMode(CENTER);
+        var pos2 = this.rain.position
+         ellipse(pos2.x,pos2.y,this.radius,this.radius)
+         
+         
+         //ellipse(this.rain.position.x,this.rain.position.y,this.radius,this.radius);
+     }
+ 
+ }
